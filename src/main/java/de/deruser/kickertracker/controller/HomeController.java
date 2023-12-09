92477e66848a63b1af685e9713909eb6e1792a1f
schajal <schajal@hlag.com>
2023-12-09 06:59:22 +0100

    test

--- src/main/java/de/deruser/kickertracker/controller/HomeController.java
+++ src/main/java/de/deruser/kickertracker/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class HomeController {
 
+    /* conflict */
     @GetMapping("/")
     public String home(){
         return "index";
